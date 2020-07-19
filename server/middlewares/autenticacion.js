1aef46372e1c4536d27648ff125e5fc4e65292cf
PeterMun <pedroanibal_jr@hotmail.com>
2020-07-19 18:42:57 -0500

    login normal seccion 10

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,71 @@
+const jwt = require('jsonwebtoken');
+
+
+//==================
+//VERIFICAR TOKEN
+//==================
+
+let verificaToken = (req, res, next) => {
+    //nombre de headers
+    let token = req.get('token'); //authorization
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+
+                ok: false,
+                err: 'Token no valido'
+
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next(); //srive para que continue con la ejecucion del programa
+
+    })
+
+
+    console.log(token);
+
+
+
+
+}
+
+//==================
+//VERIFICAR AdminRole
+//==================
+
+
+let verificaAdmin_Role = (req, res, next) => {
+
+    let usuario = req.usuario;
+
+    if (usuario.role === 'ADMIN_ROLE') {
+        next();
+
+    } else {
+
+        return res.json({
+            ok: false,
+            err: {
+                message: 'el usuario no es administrador'
+            }
+        });
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+module.exports = {
+    verificaToken,
+    verificaAdmin_Role
+}
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,12 +5,17 @@ const _ = require('underscore');
 
 
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
 
 const app = express()
 
 
-app.get('/usuario', function(req, res) {
+//app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => { //se cambio a funcion de flecha la linea anterior
+
+
+
 
     let desde = req.query.desde || 0;
 
@@ -18,7 +23,6 @@ app.get('/usuario', function(req, res) {
 
     let limite = req.query.limite || 5;
     limite = Number(limite);
-
 
     Usuario.find({ estado: true }, 'nombre email role estado google img')
         .skip(desde)
@@ -53,7 +57,7 @@ app.get('/usuario', function(req, res) {
 
 })
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let body = req.body;
 
@@ -85,7 +89,7 @@ app.post('/usuario', function(req, res) {
 
 })
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -112,7 +116,7 @@ app.put('/usuario/:id', function(req, res) {
 
 })
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
 
     let id = req.params.id;
 
